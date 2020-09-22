a11065c327aecfaf5d0f31b435e6434ea2c8fbb4
sergejij <ushakoff.sergej2010@yandex.ru>
2020-09-22 17:27:14 +0300

    app is ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Route, useHistory } from 'react-router-dom';
 
 import List from './components/List/List';
 import AddList from './components/AddList/AddList';
@@ -11,17 +12,17 @@ function App() {
   const [lists, setLists] = useState(null);
   const [colors, setColors] = useState(null);
   const [activeItem, setActiveItem] = useState(null);
+  const history = useHistory();
 
   useEffect(() => {
-    axios.get('http://localhost:3001/lists?_expand=color&_embed=tasks')
+    axios
+      .get('http://localhost:3001/lists?_expand=color&_embed=tasks')
       .then(({ data }) => {
         setLists(data);
       });
-
-    axios.get('http://localhost:3001/colors')
-      .then(({ data }) => {
-        setColors(data);
-      });
+    axios.get('http://localhost:3001/colors').then(({ data }) => {
+      setColors(data);
+    });
   }, []);
 
   const onAddList = (obj) => {
@@ -39,6 +40,43 @@ function App() {
     setLists(newList);
   };
 
+  const onRemoveTask = (listId, taskId) => {
+    if (window.confirm('Вы действительно хотите удалить задачу?')) {
+      const newList = lists.map((list) => {
+        if (list.id === listId) {
+          list.tasks = list.tasks.filter((task) => task.id !== taskId);
+        }
+        return list;
+      });
+      setLists(newList);
+      axios
+        .delete(`http://localhost:3001/tasks/${taskId}`)
+        .catch(() => alert('Не удалось удалить задачу'));
+    }
+  };
+
+  const onEditTask = (listId, taskObj) => {
+    const newTaskText = window.prompt('Текст задачи', taskObj.text);
+    if (!newTaskText) {
+      return;
+    }
+    const newList = lists.map((list) => {
+      if (list.id === listId) {
+        list.tasks = list.tasks.map((task) => {
+          if (task.id === taskObj.id) {
+            task.text = newTaskText;
+          }
+          return task;
+        });
+      }
+      return list;
+    });
+    setLists(newList);
+    axios
+      .patch(`http://localhost:3001/tasks/${taskObj.id}`, { text: newTaskText })
+      .catch(() => alert('Не удалось обновить задачу'));
+  };
+
   const onEditListTitle = (id, title) => {
     const newList = lists.map((list) => {
       if (list.id === id) {
@@ -49,13 +87,42 @@ function App() {
     setLists(newList);
   };
 
+  const onCompleteTask = (listId, taskId, completed) => {
+    const newList = lists.map((list) => {
+      if (list.id === listId) {
+        list.tasks = list.tasks.map((task) => {
+          if (task.id === taskId) {
+            task.completed = completed;
+          }
+          return task;
+        });
+      }
+      return list;
+    });
+    setLists(newList);
+    axios
+      .patch(`http://localhost:3001/tasks/${taskId}`, { completed })
+      .catch(() => alert('Не удалось обновить задачу'));
+  };
+
+  useEffect(() => {
+    const listId = history.location.pathname.split('lists/')[1];
+    if (lists) {
+      const list = lists.find((list) => list.id === Number(listId));
+      setActiveItem(list);
+    }
+  }, [lists, history.location.pathname]);
+
   return (
     <div className="todo">
       <div className="todo__sidebar">
         <List
+          onClickItem={(list) => {
+            history.push('/');
+          }}
           items={[
             {
-              active: true,
+              active: history.location.pathname === '/',
               icon: <img src={listSvg} alt="Index icon" />,
               name: 'Все задачи',
             },
@@ -66,28 +133,49 @@ function App() {
           <List
             items={lists}
             onRemove={(id) => {
-              const newLists = lists.filter((list) => list.id !== id);
+              const newLists = lists.filter((item) => item.id !== id);
               setLists(newLists);
             }}
-            onClickItem={(item) => setActiveItem(item)}
+            onClickItem={(list) => {
+              history.push(`/lists/${list.id}`);
+              setActiveItem(list);
+            }}
             activeItem={activeItem}
             isRemovable
           />
-        ) : ('Загрузка...')}
+        ) : (
+          'Загрузка...'
+        )}
         <AddList onAdd={onAddList} colors={colors} />
       </div>
       <div className="todo__tasks">
-        {
-          lists
-          && activeItem
-          && (
-          <Tasks
-            list={activeItem}
-            onAddTask={onAddTask}
-            onEditTitle={onEditListTitle}
-          />
-          )
-        }
+        <Route exact path="/">
+          {lists
+          && lists.map((list) => (
+            <Tasks
+              key={list.id}
+              list={list}
+              onAddTask={onAddTask}
+              onEditTitle={onEditListTitle}
+              onRemoveTask={onRemoveTask}
+              onEditTask={onEditTask}
+              onCompleteTask={onCompleteTask}
+              withoutEmpty
+            />
+          ))}
+        </Route>
+        <Route path="/lists/:id">
+          {lists && activeItem && (
+            <Tasks
+              list={activeItem}
+              onAddTask={onAddTask}
+              onEditTitle={onEditListTitle}
+              onRemoveTask={onRemoveTask}
+              onEditTask={onEditTask}
+              onCompleteTask={onCompleteTask}
+            />
+          )}
+        </Route>
       </div>
     </div>
   );
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 
+import Task from './Task';
 import AddTaskForm from './AddTaskFrom';
 
 import editSvg from '../../assets/img/edit.svg';
 
 import './Tasks.scss';
 
-const Tasks = ({ list, onEditTitle, onAddTask }) => {
+const Tasks = ({
+  list, onEditTitle, onAddTask, withoutEmpty, onRemoveTask, onEditTask, onCompleteTask,
+}) => {
   const editTitle = () => {
     const newTitle = window.prompt('Название списка', list.name);
     if (newTitle) {
@@ -20,45 +23,23 @@ const Tasks = ({ list, onEditTitle, onAddTask }) => {
         });
     }
   };
+
   return (
     <div className="tasks">
-      <h2 className="tasks__title">
+      <h2 style={{ color: list.color.hex }} className="tasks__title">
         {list.name}
-        <img onClick={editTitle} src={editSvg} alt="Изменить заголовок" />
+        <img onClick={editTitle} src={editSvg} alt="Edit icon" />
       </h2>
 
       <div className="tasks__items">
-        {!list.tasks.length && <h2>Задачи отсутствуют</h2>}
+        {!withoutEmpty && list.tasks && !list.tasks.length && <h2>Задачи отсутствуют</h2>}
         {
-          list.tasks.map((task) => (
-            <div key={task.id} className="tasks__items-row">
-              <div className="checkbox">
-                <div name="checkbox">
-                  <input type="checkbox" id={`task-${task.id}`} />
-                  <label htmlFor={`task-${task.id}`}>
-                    <svg
-                      width="11"
-                      height="8"
-                      viewBox="0 0 11 8"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001"
-                        stroke="#000"
-                        strokeWidth="1.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                  </label>
-                </div>
-              </div>
-              <input readOnly value={task.text} />
-            </div>
+          list.tasks
+          && list.tasks.map((task) => (
+            <Task {...task} key={task.id} list={list} onEdit={onEditTask} onRemove={onRemoveTask} onComplete={onCompleteTask} />
           ))
         }
-        <AddTaskForm list={list} onAddTask={onAddTask} />
+        <AddTaskForm key={list.id} list={list} onAddTask={onAddTask} />
       </div>
     </div>
   );
